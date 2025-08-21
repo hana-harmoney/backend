a6af6947de6eb5701c7ae73c8ee6a0b61eac3f2a
rladbflaz <youro1228@naver.com>
2025-08-21 16:58:49 +0900

    [no-issue] chore: husky 및 commitlint 설정 추가

---
+++ src/main/java/com/example/hanaharmonybackend/HanaharmonyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanaharmonybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaharmonyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaharmonyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hanaharmonybackend/HanaharmonyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanaharmonybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaharmonyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
