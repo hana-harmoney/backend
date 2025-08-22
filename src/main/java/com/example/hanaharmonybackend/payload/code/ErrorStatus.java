06cad5bd884bef4ac8261599d8a0c7043ac75747
zzimnii <102315673+zzimnii@users.noreply.github.com>
2025-08-22 12:56:10 +0900

    [#6] feat: 로그인 유저 찾기 (#8)

--- src/main/java/com/example/hanaharmonybackend/payload/code/ErrorStatus.java
+++ src/main/java/com/example/hanaharmonybackend/payload/code/ErrorStatus.java
@@ -10,8 +10,11 @@ public enum ErrorStatus implements BaseStatus {
     INVALID_INPUT("E001", "Invalid input provided", HttpStatus.BAD_REQUEST),
     SERVER_ERROR("E002", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR),
     UNAUTHORIZED("E003", "권한이 없습니다.", HttpStatus.UNAUTHORIZED),
+
+    // User 관련 error
     BAD_CREDENTIALS("E004", "아이디 또는 비밀번호가 올바르지 않습니다.", org.springframework.http.HttpStatus.UNAUTHORIZED),
-    DUPLICATE_LOGIN_ID("E005", "이미 사용 중인 아이디입니다.", HttpStatus.CONFLICT),;
+    DUPLICATE_LOGIN_ID("E005", "이미 사용 중인 아이디입니다.", HttpStatus.CONFLICT),
+    USER_NOT_FOUND("E006", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String message;
