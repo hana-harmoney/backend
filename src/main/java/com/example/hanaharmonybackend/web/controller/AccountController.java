11fa19a584614d5db2678a5a4e92470ffe55d555
youlimsongs <giuhki@gmail.com>
2025-08-25 13:10:29 +0900

    [#5] feat: 계좌서비스 인터페이스, 구현체 분리

--- src/main/java/com/example/hanaharmonybackend/web/controller/AccountController.java
+++ src/main/java/com/example/hanaharmonybackend/web/controller/AccountController.java
@@ -6,7 +6,6 @@ import com.example.hanaharmonybackend.service.AccountService;
 import com.example.hanaharmonybackend.util.SecurityUtil;
 import com.example.hanaharmonybackend.web.dto.account.AccountResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
